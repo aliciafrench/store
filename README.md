37282d7a2c3871d5837b913fa0c2c9543573b5ef
Alicia French <alienor18@yahoo.com>
2014-02-02 22:34:11 -0800

    Finished Railscast #417

---
+++ README.md
@@ -0,0 +1,5 @@
+## Description
+
+* This is a basic application that allows to register new products on an e-commerce site.
+* It is based on [RailsCast #417](http://railscasts.com/episodes/417-foundation?view=asciicast).
+* It is an exercise for a Rails class that I am following.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '082be6a0c0c9fae500cca407d1550074174744a2bfdddf4bebe9eb5d94d8af5d1758abbf17b5dab42624b32ecf66532c3a58d51fec3ce3a6366dfdd12764f3db'
